b05fc6b5a2e0bb60643f7bac6f821e910a40575b
Chris-AM <c.aranguizm@outlook.com>
2024-01-11 15:52:35 -0300

    :sparkles: adding path to components

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../presentation/pages/home/home.component';
+import { AboutComponent } from '../presentation/pages/about/about.component';
+import { ProjectsComponent } from '../presentation/pages/projects/projects.component';
+import { ArticlesComponent } from '../presentation/pages/articles/articles.component';
+import { ContactComponent } from '../presentation/pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
