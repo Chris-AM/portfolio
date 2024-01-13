81d87e979d6ba70851d0e1a723cdd8a0bf7c900a
Chris-AM <c.aranguizm@outlook.com>
2024-01-12 23:19:03 -0300

    :sparkles: home logo is ready

--- src/app/presentation/pages/home/home-logo/home-logo.component.ts
+++ src/app/presentation/pages/home/home-logo/home-logo.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { HomeData } from '../../../../domain/domain-index';
 
 @Component({
   selector: 'app-home-logo',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './home-logo.component.html',
   styleUrl: './home-logo.component.css',
 })
 export class HomeLogoComponent {
   @Input() homeData: HomeData = {} as HomeData;
+  @Input() logoSize = 0;
 }
--- src/app/presentation/pages/home/home.component.ts
+++ src/app/presentation/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  HostListener,
   OnInit,
   inject,
   signal,
@@ -19,9 +20,46 @@ import { HomeLogoComponent } from './home-logo/home-logo.component';
 })
 export class HomeComponent implements OnInit {
   public homeData = signal<HomeData>({} as HomeData);
+  public logoSize = signal<number>(100);
+  public oldLogoSize = signal<number>(80);
+  public stayAtTop = signal<boolean>(false);
+
   private readonly homeService: HomeService = inject(HomeService);
+
   ngOnInit(): void {
     this.getHomeData();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scroll = Math.round(window.scrollY);
+    const newLogoSize = 80 - (scroll * 4) / 2;
+    if (newLogoSize < this.oldLogoSize()) {
+      if (newLogoSize > 40) {
+        this.logoSize.set(newLogoSize);
+        this.oldLogoSize.set(newLogoSize);
+        this.stayAtTop.set(false);
+      } else {
+        this.stayAtTop.set(true);
+      }
+    } else {
+      this.logoSize.set(newLogoSize);
+      this.stayAtTop.set(false);
+    }
+  }
+
+  public logoStyle() {
+    return {
+      display: 'flex',
+      position: this.stayAtTop() ? 'fixed' : 'relative',
+      top: this.stayAtTop() ? '3vh' : 'auto',
+      zIndex: this.stayAtTop() ? '999' : 'auto',
+      border: this.stayAtTop() ? '1px solid black' : 'none',
+      borderRadius: this.stayAtTop() ? '50%' : 'none',
+      boxShadow: this.stayAtTop()
+        ? '0px 0px 10px 0px rgba(0,0,0,0.75)'
+        : 'none',
+    };
   }
 
   private getHomeData(): void {
