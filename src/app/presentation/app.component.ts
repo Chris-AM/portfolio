319e5a49bd34fd8dd16f20397c38bfa7f5df030e
Chris-AM <c.aranguizm@outlook.com>
2024-01-12 23:18:09 -0300

    :construction: building app presentation

--- src/app/presentation/app.component.ts
+++ src/app/presentation/app.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FloatingNavbarComponent } from './shared/floating-navbar/floating-navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, FloatingNavbarComponent],
+  imports: [RouterOutlet, FloatingNavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
