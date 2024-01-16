13f3d412145a6300165debdc0f2c546604e767cb
Chris-AM <c.aranguizm@outlook.com>
2024-01-16 03:11:26 -0300

    :sparkles: adding social media to home

--- src/app/presentation/shared/social-media/social-media.component.ts
+++ src/app/presentation/shared/social-media/social-media.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { SocialMedia } from '../../../domain/domain-index';
+import { SocialMediaService } from '../../../use-cases/social-media/social-media.service';
 
 @Component({
   selector: 'app-social-media',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './social-media.component.html',
-  styleUrl: './social-media.component.css'
+  styleUrl: './social-media.component.css',
 })
-export class SocialMediaComponent {
+export class SocialMediaComponent implements OnInit {
+  @Input() showJustIcons = false;
+  public socialMedia = signal<SocialMedia[]>([]);
 
+  constructor(private readonly service: SocialMediaService) {}
+
+  ngOnInit(): void {
+    this.getSocialMedia();
+  }
+
+  public copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text);
+  }
+
+  private getSocialMedia(): void {
+    this.service.getSocialMedia().subscribe({
+      next: (data) => {
+        this.socialMedia.set(data);
+      },
+    });
+  }
 }
