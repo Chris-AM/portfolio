3327117929a1bb8c244eb46d026615f7e5921689
Chris-AM <c.aranguizm@outlook.com>
2024-01-18 20:07:16 -0300

    :bricks: declaring references architecture

--- src/app/infrastructure/datasources/references.datasource.impl.ts
+++ src/app/infrastructure/datasources/references.datasource.impl.ts
@@ -1,11 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../config/environment.config';
-import { ReferenceModel } from '../../domain/domain-index';
+import {
+  ReferenceModel,
+  ReferencesDataSource,
+} from '../../domain/domain-index';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export abstract class ReferencesDataSourceImpl implements ReferenceModel {
+export abstract class ReferencesDataSourceImpl implements ReferencesDataSource {
   private readonly baseUrl = environment.base_url;
 
   constructor(private readonly http: HttpClient) {}
+
+  getReferences(): Observable<ReferenceModel[]> {
+    const petition = this.http.get<ReferenceModel[]>(
+      `${this.baseUrl}${environment.references}`,
+    );
+    return petition;
+  }
 }
